166d564e83d63cd43bd95b0f046c152864e14378
Alfin Adzuhri Delangga Putra <zurapro21@gmail.com>
2024-10-23 21:10:31 +0700

    feat(class): test commitizen
    i dont now

--- main.go
+++ main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/alfinadzuhri/practice-crud-golang/config"
 )
 
 func main() {
 	fmt.Println("Niggaaa")
 
-	cfg, err := config.NewConfig()
+	// cfg, err := config.NewConfig()
 
-	fmt.Println(cfg)
-	fmt.Println(err)
+	// fmt.Println(cfg)
+	// fmt.Println(err)
 }
